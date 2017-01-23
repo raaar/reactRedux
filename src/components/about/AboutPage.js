8fdee50d04026af41b9dd4c84e70a473ee87c263
Raf <raf.f@hotmail.co.uk>
2017-01-23 21:20:04 +0700

    boilerplate setup, and first components

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
